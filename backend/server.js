e0e7b163c48e8e3f1d29ff6b714597b85ea8ee82
Sourav <tonalmango@gmail.com>
2026-01-06 20:35:49 +0530

    TRIGGER REDEPLOY: MongoDB whitelist updated, reconnecting to database

--- backend/server.js
+++ backend/server.js
@@ -95,6 +95,7 @@ app.use((req, res, next) => {
 
 // Connect to MongoDB
 console.log('Attempting MongoDB connection...');
+console.log('MongoDB URI configured:', process.env.MONGODB_URI ? 'Yes' : 'No');
 try {
     mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/corelogiclabs', {
         useNewUrlParser: true,
